05ae98d175328ea7ed265d9920a642bcd738a7a9
joaodafi <joao.miguel.bb@hotmail.com>
2021-08-03 15:35:04 -0300

    ft_bzero.c is stupid

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,28 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jomiguel < jomiguel@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/03 11:03:35 by jomiguel          #+#    #+#             */
-/*   Updated: 2021/08/03 15:21:54 by jomiguel         ###   ########.fr       */
+/*   Updated: 2021/08/03 15:25:02 by jomiguel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *str, int new_char, size_t how_many)
+void	ft_bzero(void *str, size_t how_many)
 {
-	size_t			counter;
-	unsigned char	*holder;
-
-	counter = 0;
-	holder = (unsigned char *)str;
-	while (counter < how_many)
-	{
-		holder[counter] = new_char;
-		counter++;
-	}
-	return (str);
+	ft_memset(str, '0', how_many);
 }
--- libft.h
+++ libft.h
@@ -1,14 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jomiguel < jomiguel@student.42sp.org.br    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/03 14:42:06 by jomiguel          #+#    #+#             */
+/*   Updated: 2021/08/03 15:13:15 by jomiguel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBFT_H
 # define LIBFT_H
 
 # include <stdlib.h>
 
-int	ft_isdigit(int c);
-int	ft_isalpha(int c);
-int	ft_isalnum(int c);
-int	ft_isprint(int c);
-int	ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isalpha(int c);
+int		ft_isalnum(int c);
+int		ft_isprint(int c);
+int		ft_isascii(int c);
 size_t	ft_strlen(const char *str);
-void	*ft_menset(void *s, int c, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *str, size_t how_many);
 
 #endif
