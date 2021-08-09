4921a6d304ae64503139ae53b1041854d5a900cb
joaodafi <joao.miguel.bb@hotmail.com>
2021-08-09 10:30:02 -0300

    minor changes on dtrlcpy and strlen

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: jomiguel < jomiguel@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/05 12:06:01 by jomiguel          #+#    #+#             */
-/*   Updated: 2021/08/05 15:47:57 by jomiguel         ###   ########.fr       */
+/*   Updated: 2021/08/05 17:07:55 by jomiguel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,15 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	size_t	counter;
 	size_t	src_len;
 
-	counter = 0;
-	src_len = ft_strlen (src);
 	if (!dst || !src)
 		return (0);
+	src_len = ft_strlen (src);
+	if (!size)
+		return (src_len);
+	counter = 0;
 	if (size < 0)
 	{
-		while ((counter < size) && (src[counter]))
+		while ((counter < size -1) && src[counter])
 		{
 			dst[counter] = src[counter];
 			counter++;
--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: jomiguel < jomiguel@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/02 11:45:28 by jomiguel          #+#    #+#             */
-/*   Updated: 2021/08/05 12:20:29 by jomiguel         ###   ########.fr       */
+/*   Updated: 2021/08/05 16:36:26 by jomiguel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ size_t	ft_strlen(const char *s)
 {
 	size_t	len;
 
+	if (!s)
+		return (0);
 	len = 0;
 	while (s[len])
 		len ++;
