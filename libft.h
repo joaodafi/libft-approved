4138a9e68d8a21132a65a4b5635b5c05adcf2a04
joaodafi <joao.miguel.bb@hotmail.com>
2021-08-13 14:19:53 -0300

    strncmp done

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jomiguel < jomiguel@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/03 14:42:06 by jomiguel          #+#    #+#             */
-/*   Updated: 2021/08/13 11:16:34 by jomiguel         ###   ########.fr       */
+/*   Updated: 2021/08/13 14:18:11 by jomiguel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,5 +31,6 @@ int		ft_toupper(int c);
 int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
