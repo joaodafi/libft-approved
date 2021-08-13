7fcb16cd265adb3c8fb26d92ad01b2605ad97318
joaodafi <joao.miguel.bb@hotmail.com>
2021-08-13 14:47:41 -0300

    memchr done

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,25 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jomiguel < jomiguel@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/13 11:58:20 by jomiguel          #+#    #+#             */
-/*   Updated: 2021/08/13 14:31:11 by jomiguel         ###   ########.fr       */
+/*   Created: 2021/08/13 14:27:04 by jomiguel          #+#    #+#             */
+/*   Updated: 2021/08/13 14:40:46 by jomiguel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t cntr;
+	size_t counter;
 
-	cntr = 0;
-	if (n == cntr)
-		return (0);
-	while (cntr < n && s1[cntr] == s2[cntr] && s1[cntr] && s2[cntr])
-		cntr++;
-	return ((unsigned char)s1[cntr] - (unsigned char)s2[cntr]);
+	counter = 0;
+	while (counter < n)
+	{
+		if (((unsigned char *)s)[counter] == (unsigned char)c)
+			return ((void *)(s + counter));
+		counter++;
+	}
+	return (0);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jomiguel < jomiguel@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/03 14:42:06 by jomiguel          #+#    #+#             */
-/*   Updated: 2021/08/13 14:18:11 by jomiguel         ###   ########.fr       */
+/*   Updated: 2021/08/13 14:43:58 by jomiguel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,5 +32,6 @@ int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 
 #endif
