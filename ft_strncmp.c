bcc71939c72006a600155d01b38e0d4825cc0c37
joaodafi <joao.miguel.bb@hotmail.com>
2021-09-18 21:50:26 +0000

    aaa

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jomiguel < jomiguel@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/13 11:58:20 by jomiguel          #+#    #+#             */
-/*   Updated: 2021/09/18 21:25:29 by jomiguel         ###   ########.fr       */
+/*   Updated: 2021/09/18 21:50:04 by jomiguel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	cntr = 0;
 	if (n == cntr)
 		return (0);
-	while (cntr < (n + 1) && s1[cntr] == s2[cntr] && s1[cntr] && s2[cntr])
+	while ((cntr + 1) < n && s1[cntr] == s2[cntr] && s1[cntr] && s2[cntr])
 		cntr++;
 	return ((unsigned char)s1[cntr] - (unsigned char)s2[cntr]);
 }
